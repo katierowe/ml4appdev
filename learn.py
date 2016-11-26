3e2c9f8c7bdab392cf56dbe8deffa6ca8e9f04d9
Dan Bikle <bikle@bikle.com>
2016-11-25 17:53:48 -0800

    hello

--- learn.py
+++ learn.py
@@ -3,23 +3,27 @@
 import pandas as pd
 import pdb
 # This script should learn from /tmp/iris_train.csv
+# I should assume that f1 column depends on f0 column.
 
 train_df = pd.read_csv('/tmp/iris_train.csv')
 
-# I should collect independent variables in a list:
+# I should collect independent variables in a nested list:
 x_l = [[x_f] for x_f in train_df.f0]
-print(x_l[:9])
 
-# I should import linear_model
+# I should import linear_model:
 from sklearn import linear_model
 linr_model = linear_model.LinearRegression()
+# I should call fit() to create the model:
 linr_model.fit(x_l, train_df.f1)
-# That was easy. I needed only 3 lines of syntax.
 
-print('scikit-learn calculates W to be:')
-print(linr_model.coef_[0])
-print('scikit-learn calculates b to be:')
-print(linr_model.intercept_)
+# I should save the model:
 
+w0_f = linr_model.intercept_
+w_l  = linr_model.coef_
+m_l  = [w0_f] + w_l.tolist()
+m_sr = pd.Series(m_l)
+m_s  = '/tmp/iris_model.csv'
+m_sr.to_csv(m_s, float_format='%4.4f', index=False, header=['weights'])
+print('Model saved to: '+ '/tmp/iris_model.csv')
 'bye'
 

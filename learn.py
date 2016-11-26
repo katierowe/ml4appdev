e7441c8431fd18d547ae3fb53e2d714303f47168
Dan Bikle <bikle@bikle.com>
2016-11-25 16:58:13 -0800

    hello

---
+++ learn.py
@@ -0,0 +1,11 @@
+# learn.py
+
+import pandas as pd
+import pdb
+# This script should learn from /tmp/iris_train.csv
+train_df = pd.read_csv('/tmp/iris_train.csv')
+
+print(train_df.head())
+
+'bye'
+
---
+++ slice_learn_predict.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# slice_learn_predict.bash
+
+# This script should slice iris.csv into /tmp/iris_train.csv and /tmp/iris_test.csv
+
+# It should learn from /tmp/iris_train.csv
+# It should predict f1 values from f0 values in /tmp/iris_test.csv
+# It should compare predicted f1 values from observed f1 values in /tmp/iris_test.csv
+
+python slice_train_test.py
+python learn.py
+python predict.py
+python compare.py
+
+
+exit
+
+

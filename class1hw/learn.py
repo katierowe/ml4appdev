c66c2732f0e77eaca6ad4a9746ce5cda22e5997e
Katie Rowe <katie@siluriatech.com>
2017-12-10 09:51:56 -0800

    Reformatted learn.py to be a bit cleaner.

--- class1hw/learn.py
+++ class1hw/learn.py
@@ -1,12 +1,15 @@
-# learn.py
+"""
+learn.py
+
+This script learns from /tmp/iris_train.csv. Based on the y_variable name it will
+determine whether a regression or classification model should be used.
+"""
 
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import cPickle as pickle
 
-# This script should learn from /tmp/iris_train.csv
-# I should assume that f1 column depends on f0 column.
 
 def dump_to_pickle(model, file_path):
     with open(file_path, 'wb') as output:
@@ -14,45 +17,19 @@ def dump_to_pickle(model, file_path):
         pickler.dump(model)
 
 
-train_df = pd.read_csv('/tmp/iris_train.csv')
-index = list(['intercept']) + list(train_df.columns)
-columns = list(['intercept', 'f0', 'f1', 'f2', 'f3',
-              'iris_type_0', 'iris_type_1','iris_type_2'])
+if __name__ == '__main__':
+    train_df = pd.read_csv('/tmp/iris_train.csv')
 
-model_df = pd.DataFrame(index=index,
-                        columns=columns)
+    for y_var in train_df.columns:
+        coeff_list = [col for col in train_df.columns if col != y_var]
+        # I should collect independent variables in a nested list:
+        X = np.array(train_df[coeff_list])
 
-for var in train_df.columns:
-    coeff_list = [col for col in train_df.columns if col != var]
-    # I should collect independent variables in a nested list:
-    x_l = np.array(train_df[coeff_list])
+        if y_var == 'iris_type':
+            model = linear_model.SGDClassifier()
+        else:
+            model = linear_model.LinearRegression()
 
-    if var == 'iris_type':
-        model = linear_model.SGDClassifier()
-        model.fit(x_l, train_df[var])
-        dump_to_pickle(model, '/tmp/' + var + '_model.pkl')
-        #for i in range(0,3):
-        #    model_df.loc[['f0','f1','f2','f3'], 'iris_type_{:.0f}'.format(i)] = model.coef_[i]
-        #    model_df.loc['intercept', 'iris_type_{:.0f}'.format(i)] = model.intercept_[i]
-    else:
-        # I should import linear_model:
-        model = linear_model.LinearRegression()
-
-
-        # I should call fit() to create the model:
-        model.fit(x_l, train_df[var])
-        dump_to_pickle(model, '/tmp/' + var + '_model.pkl')
-        # I should save the model:
-        #w0_f = model.intercept_
-        ##w_l  = model.coef_
-        #m_l  = [w0_f] + w_l.tolist()
-        #m_sr = pd.Series(index=list(['intercept']) + coeff_list, data=m_l)
-        #model_df[var] = m_sr
-'''
-model_df = model_df.fillna(0)
-model_df.index.name = 'model_term'
-model_filepath  = '/tmp/iris_model.csv'
-model_df.to_csv(model_filepath, index_label='model_term')
-print('Model saved to: '+ '/tmp/iris_model.csv')
-
-'''
+            # I should call fit() to create the model:
+        model.fit(X, train_df[y_var])
+        dump_to_pickle(model, '/tmp/' + y_var + '_model.pkl')
--- class1hw/predict.py
+++ class1hw/predict.py
@@ -3,7 +3,7 @@
 # This script should use the model in /tmp/iris_model.csv to predict X-values in /tmp/iris_test.csv
 
 import pandas as pd
-import numpy  as np
+import numpy as np
 import cPickle as pickle
 
 def read_from_pickle(file_path):
@@ -13,34 +13,13 @@ def read_from_pickle(file_path):
 # I should get the data I want to predict:
 test_df = pd.read_csv('/tmp/iris_test.csv')
 terms_to_predict = test_df.columns
-# I should get the model I created earlier:
-#model_df = pd.read_csv('/tmp/iris_model.csv', index_col='model_term')
-#model_terms = model_df.index.values
-#print model_df
-
-# Setting up the intercept column and filling with ones.
-#test_df['intercept'] = np.ones(len(test_df))
-#data_slice = np.array(test_df[model_terms])
 
 for term_predicted in terms_to_predict:
     data_slice = np.array(test_df[[term for term in terms_to_predict if term != term_predicted]])
     model = read_from_pickle('/tmp/' + term_predicted + '_model.pkl')
     predictions = model.predict(data_slice)
     test_df[term_predicted + '_prediction'] = predictions
-    '''if term_predicted != 'iris_type':
-        # To use Linear Algebra to generate predictions from model and test-data, I matrix-multiply:
-        weight_array = np.array(model_df.loc[:, term_predicted])
 
-        predictions_a = np.matmul(data_slice, weight_array)
-
-        # I should write the predictions to CSV in a way which helps me compare then to observations:
-        test_df[term_predicted + '_prediction'] = predictions_a
-
-    else:
-        model_slice = model_df[[c for c in model_df.columns if 'iris_type' in c]]
-        for i in range(0, 3):
-            print test_df['iris_type']
-            print np.matmul(test_df[model_terms], model_slice)'''
 
 print test_df
 test_df.to_csv('/tmp/iris_predictions.csv', float_format='%4.2f', index=False)
